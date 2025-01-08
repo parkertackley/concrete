1499425f0f7c45ad1e9035805e54fa6d19f0380b
parkertackley <parkertackley@gmail.com>
2025-01-08 12:55:15 -0500

    feat: colorful digits

--- src/outputfile.c
+++ src/outputfile.c
@@ -49,7 +49,16 @@ void editorDrawsRows(struct abuf *ab) {
                 len = 0;
             if(len > E.screencols)
                 len = E.screencols;
-            abAppend(ab, &E.row[filerow].render[E.coloff], len);
+                char *c = &E.row[filerow].render[E.coloff];
+                for(int j = 0; j < len; j++) {
+                    if(isdigit(c[j])) {
+                        abAppend(ab, "\x1b[31m", 5);
+                        abAppend(ab, &c[j], 1);
+                        abAppend(ab, "\x1b[39m", 5);
+                    } else {
+                        abAppend(ab, &c[j], 1);
+                    }
+                }
         }
         
 
--- src/outputfile.h
+++ src/outputfile.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <ctype.h>
 
 void editorScroll();
 void editorDrawsRows(struct abuf *ab);
